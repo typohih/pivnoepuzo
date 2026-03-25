aefe11ccfe7f1ee715ff0378bd1d1ca33d693035
typohih <johnwalksonthestreet@gmail.com>
2026-03-25 15:19:35 +0700

    Update expert styles and mobile layout

--- Pivo/src/Experts/Experts.jsx
+++ Pivo/src/Experts/Experts.jsx
@@ -5,6 +5,7 @@ import myAccount17200091 from '../../images/free-icon-my-account-17200091.png'
 import photo20241124030027 from '../../images/photo_2024-11-24_03-00-27.jpg'
 import photo20260324085047 from '../../images/photo_2026-03-24_08-50-47.jpg'
 import Volodya from '../../images/photo_2026-03-25_12-53-49.jpg'
+import Ivan from '../../images/Ivan.PNG'
 
 export function Experts () {
 
@@ -48,6 +49,20 @@ export function Experts () {
                 </div>
                 <div className='hidden_Expert3'>
                     <img src={Volodya} alt="" />
+                </div>
+            </div>
+
+            <div className='Expert4'>
+                Никита Карамазов
+                <img src={myAccount17200091} alt="" />
+                <div className='hidden_content'>
+                    <img src={beer931949} alt="" />
+                </div>
+                <div className='hidden_content2'>
+                    <img src={beer2977173} alt="" />
+                </div>
+                <div className='hidden_Expert4'>
+                    <img src={Ivan} alt="" />
                 </div>
             </div>
 
